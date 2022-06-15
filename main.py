406fee70b993ba46d3e5bb5f68e43aa5fe12cd7a
hamon <qtbelt@gmail.com>
2022-06-15 13:17:25 +0900

    변경) 파일 작성 방식 추가) 주석판정

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import os
 import re
 
 totalFileCnt = 0
+f2 = open('C:/BACK/FilePile/korean.csv', 'w')
+nia = open('C:/BACK/FilePile/fileList.csv', 'w')
+
 def print_files_in_dir(root_dir, prefix):
     files = os.listdir(root_dir)
     exceptFileExt = ['.js', '.jsp', '.java', '.xml']
@@ -17,11 +20,10 @@ def print_files_in_dir(root_dir, prefix):
                 #print(prefix + path)
                 fileRead(path)
 
+
 def fileRead(path):
     print(path)
     f = open(path, 'r', encoding="UTF-8")
-    f2 = open(csv_dir, 'a')
-    nia = open(nia_dir, 'a')
     # fResult = open(result_dir, 'a')
     f2.write(f'{path}\n')
     nia.write(f'{path}\n')
@@ -45,33 +47,37 @@ def fileRead(path):
         if afterStr.startswith('/*'):continue
         if afterStr.startswith('/**'):continue
 
-        slNo = afterStr.find('//')
-        coreLine = afterStr[:slNo]
+        slNo1 = afterStr.find('//')
+        tempLine1 = afterStr[:slNo1]
+        slNo2 = tempLine1.find('/*')
+        tempLine2 = tempLine1[:slNo2]
+        slNo3 = tempLine2.find('<!-')
+        coreLine = tempLine2[:slNo3]
+
         result = hangul.search(coreLine)
         if result is not None:
             result2 = notHangul.sub('', coreLine).lstrip().rstrip()
             lineStr = afterStr.replace(',',' ').replace('•',' ').replace('₩','$')
 
-            if afterStr.startswith('<%'):
-                print(f'NIA {lineNo}: {lineStr} -> {result2} ')
-                nia.write(f'{lineNo},{lineStr},{result2}\n')
-                continue
+                # print(f'NIA {lineNo}: {lineStr} -> {result2} ')
+                # nia.write(f'{lineNo},{lineStr},{result2}\n')
+
 
             print(f'{lineNo}: {lineStr} -> {result2} ')
             f2.write(f'{lineNo},{lineStr},{result2}\n')
             # fResult.write(f'{lineNo}, {result2}\n')
             #todo 파일에 쓰기
     f.close()
-    f2.close()
     # fResult.close()
 
 if __name__ == "__main__":
     #root_dir = 'E:\\webproject-site\\NetisWeb_last\\netis\\src\\main\\webapp\WEB-INF'
-    root_dir = 'D:\\hamon\\trunk\\netis\\src\\main\\java\\com\\hm\\netis\\webapp\\main\\widget' #불러올 파일 경로
-    csv_dir = 'C:/BACK/FilePile/korean.csv' #저장할 파일 경로
-    nia_dir = 'C:/BACK/FilePile/nia.csv' #저장할 파일 경로
+    root_dir = 'D:\\hamon\\trunk' #불러올 파일 경로
     colE_dir = 'C:/BACK/FilePile/error.csv'
     # result_dir = 'C:/BACK/FilePile/result.csv'
     hangul = re.compile('[ㄱ-ㅣ가-힣]')
     notHangul = re.compile('[^ ㄱ-ㅣ가-힣|()+]')
     print_files_in_dir(root_dir, "")
+
+    f2.close()
+    nia.close()
